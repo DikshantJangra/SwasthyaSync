1439addccdec73727fd8fac5db34c59744af1915
DikshantJangra <dikshantjangra1@gmail.com>
2025-05-03 21:14:09 +0530

    Dashboard top ✅

--- app/Hydration/page.tsx
+++ app/Hydration/page.tsx
@@ -1,13 +1,32 @@
 import Link from 'next/link'
 import React from 'react'
+import { IoBodySharp } from "react-icons/io5";
 
 const Hydration = () => {
   return (
-    <div className='px-4 pt-5'>
-        <p className='text-4xl font-bold'>today</p>
-        <div>
-            {/* <Link href={'/'}>daily</Link> */}
+    <div className='h-screen px-4 pt-5'>
+      <div className='grid grid-cols-[15%_85%]'>
+        <div className='flex gap-3'>
+          <img src="/swasthyasync.svg" alt="Swasthya Sync" />
+          <p className='font-black text-3xl text-[#FF4A20] tracking-tight leading-none'>Swāsthya <br /> Sync</p>
         </div>
+        <p className='self-end justify-self-center text-5xl'>
+          <span>Welcome </span>
+          <span className='font-bold text-[#FF4A20]'>Dikshant!</span>
+        </p>
+      </div>
+
+      <div className=''>
+
+      </div>
+      {/* 
+      <div className='grid grid-cols-[5%_95%] grid-rows-1'>
+        <div className='bg-zinc-200'>
+          <Link href={'/'}><IoBodySharp /></Link>
+        </div>H
+        <div className='bg-red-600'></div>
+      </div>
+      */}
     </div>
   )
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <header className='py-5 px-4'>
         <nav className='flex items-end gap-5'>
             <Image src={'/swasthyasync.png'} height={40} width={40} alt='Swasthya Sync'></Image>
-            <span className='text-4xl font-black'>Swasthya Sync</span>
+            <span className='text-4xl font-black'>Swāsthya Sync</span>
         </nav>
     </header>
   )
